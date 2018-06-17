48a677ffee0c463bfe9e9dfecf948acda82c12b7
songxiao <lovebirdsx@gmail.com>
2018-06-18 03:57:34 +0800

    Minimap up

---
+++ Assets/TestMinimap/SnapshotCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class SnapshotCamera : MonoBehaviour {
+	
+	void Start() {
+		StartCoroutine(_WaitAndDestory());
+	}
+
+	IEnumerator _WaitAndDestory() {
+		for (int i = 0; i < 3; i++) {
+			yield return new WaitForFixedUpdate();
+		}
+		Camera camera = GetComponent<Camera>();
+		camera.targetTexture = null;
+		Destroy(gameObject);
+	}
+}
